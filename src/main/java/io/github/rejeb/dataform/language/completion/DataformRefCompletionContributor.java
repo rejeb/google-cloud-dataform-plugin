bbb6d76733b7215cc0e670bbe2de453143ffc517
rbenrejeb <6606952+rejeb@users.noreply.github.com>
2026-07-25 10:46:27 +0200

    Fix tables autocomplete in ref

--- src/main/java/io/github/rejeb/dataform/language/completion/DataformRefCompletionContributor.java
+++ src/main/java/io/github/rejeb/dataform/language/completion/DataformRefCompletionContributor.java
@@ -56,6 +56,7 @@ public class DataformRefCompletionContributor extends CompletionContributor {
                     result.addElement(lookupElement);
                 }
             }
+            result.stopHere();
         }
 
         @Nullable
